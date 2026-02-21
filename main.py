dd7d6f377eda69038fee90fc5588ad71742c6586
thunchanok tangpong <thunchanoktangpong@thunchanoks-MacBook-Pro.local>
2026-02-21 22:14:31 +0700

    remove print apikey

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import os
-
 from dotenv import load_dotenv
-
 load_dotenv()
-
 
 def main():
     print("Hello from langchain-course!")
-    print(os.getenv("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
